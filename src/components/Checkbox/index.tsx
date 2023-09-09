bcb624c1b1b2c8276c1b0781cf2681ebe0fdba01
adejolx <dejoladaniel@gmail.com>
2023-09-09 08:57:00 +0100

    refactor: checkbox
    removed redundant type

--- src/components/Checkbox/index.tsx
+++ src/components/Checkbox/index.tsx
@@ -3,16 +3,18 @@ import * as CheckboxPrimitive from '@radix-ui/react-checkbox';
 import { Check } from 'lucide-react';
 import { cn } from 'utils';
 
-type CheckboxProps = {
-  checked?: boolean | 'indeterminate';
-  required?: boolean;
-  onCheckedChange?: (checked: boolean | 'indeterminate') => void;
-  className?: string;
-};
+const ICON_SIZE = 16;
+const ICON_STROKE_WIDTH = 3;
+
+const DEFAULT_CHECKBOX_CLASSES =
+  'bg-white w-[--checkbox-size] h-[--checkbox-size] border-gray-300/70 border-[length:--checkbox-border-width] rounded-[--checkbox-border-radius] flex items-center justify-center [&[data-state=checked]:not([data-disabled])]:border-blue-300 [&[data-state=indeterminate]:not([data-disabled])]:border-blue-300 [&:not([data-disabled])]:hover:outline outline-2 outline-blue-100 outline-offset-1 [&:not([data-disabled])]:focus:outline outline-2 outline-blue-100 outline-offset-1';
+
+const DEFAULT_CHECKBOX_INDICATOR_CLASSES =
+  'text-white [&[data-state=checked]:not([data-disabled])]:bg-blue-300 [&[data-state=checked]:not([data-disabled])]:w-full [&[data-state=checked]:not([data-disabled])]:h-full [&[data-state=indeterminate]:not([data-disabled])]:bg-blue-300 [&[data-state=indeterminate]:not([data-disabled])]:w-full [&[data-state=indeterminate]:not([data-disabled])]:h-full [&[data-state=checked][data-disabled]]:bg-gray-100 [&[data-state=indeterminate][data-disabled]]:bg-gray-100';
 
 const Checkbox = forwardRef<
   React.ElementRef<typeof CheckboxPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof CheckboxPrimitive.Root> & CheckboxProps
+  React.ComponentPropsWithoutRef<typeof CheckboxPrimitive.Root>
 >(
   (
     { checked, required = false, onCheckedChange, className, ...props },
@@ -20,12 +22,6 @@ const Checkbox = forwardRef<
   ) => {
     const [isTouched, setIsTouched] = useState(false);
     const [defaultChecked, setdefaultChecked] = useState(false);
-    const ICON_SIZE = 16;
-    const ICON_STROKE_WIDTH = 3;
-    const DEFAULT_CHECKBOX_CLASSES =
-      'bg-white w-[--checkbox-size] h-[--checkbox-size] border-gray-300/70 border-[length:--checkbox-border-width] rounded-[--checkbox-border-radius] flex items-center justify-center [&[data-state=checked]:not([data-disabled])]:border-blue-300 [&[data-state=indeterminate]:not([data-disabled])]:border-blue-300 [&:not([data-disabled])]:hover:outline outline-2 outline-blue-100 outline-offset-1 [&:not([data-disabled])]:focus:outline outline-2 outline-blue-100 outline-offset-1';
-    const DEFAULT_CHECKBOX_INDICATOR_CLASSES =
-      'text-white [&[data-state=checked]:not([data-disabled])]:bg-blue-300 [&[data-state=checked]:not([data-disabled])]:w-full [&[data-state=checked]:not([data-disabled])]:h-full [&[data-state=indeterminate]:not([data-disabled])]:bg-blue-300 [&[data-state=indeterminate]:not([data-disabled])]:w-full [&[data-state=indeterminate]:not([data-disabled])]:h-full [&[data-state=checked][data-disabled]]:bg-gray-100 [&[data-state=indeterminate][data-disabled]]:bg-gray-100';
     const ERROR_CLASSES =
       required && (!checked || !defaultChecked) && isTouched ? 'required' : '';
 
