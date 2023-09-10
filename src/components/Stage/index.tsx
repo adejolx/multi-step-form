52a595a32b05ce964956b0309134a1e642315ef8
adejolx <dejoladaniel@gmail.com>
2023-09-10 10:15:53 +0100

    feat: add stage component

--- src/components/Stage/index.tsx
+++ src/components/Stage/index.tsx
@@ -1,4 +1,22 @@
-const Stage = () => {
-    return<></>
+interface StageProps {
+  step: number;
+  title: string;
+  onStepChange: () => void;
 }
-export {Stage}
+const Stage: React.FC<StageProps> = ({ step, title, onStepChange }) => {
+  return (
+    <div className="flex items-center gap-4 text-gray-200">
+      <button
+        className="w-10 h-10 rounded-full border-[1.5px] border-gray-200 font-bold hover:bg-blue-100 hover:text-blue-400 transition-colors"
+        onClick={onStepChange}
+      >
+        {step}
+      </button>
+      <p className="sr-only md:not-sr-only flex flex-col gap-0">
+        <span className="text-sm">Step {step}</span>
+        <span className="font-bold uppercase">{title}</span>
+      </p>
+    </div>
+  );
+};
+export { Stage };
