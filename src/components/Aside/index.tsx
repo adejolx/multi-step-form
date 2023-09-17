90318b7dcbdc965902d90e223e112f151c4f8400
adejolx <dejoladaniel@gmail.com>
2023-09-18 00:02:03 +0100

    style: add styles and extend utility classes

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -32,29 +32,37 @@ const Aside = ({ className }: AsideProps) => {
   const inputIsValid = checkInputValidity(userInfo, errors);
 
   return (
-    <ul
+    <div
       className={cn(
-        'flex justify-center gap-4 bg-blue-300 bg-sidebarMobile bg-origin-border bg-center bg-no-repeat bg-cover md:block md:space-y-8 md:bg-sidebarDesktop px-8 pt-8 pb-12',
+        'bg-blue-300 bg-sidebarMobile bg-origin-border bg-center bg-no-repeat bg-cover md:bg-sidebarDesktop px-8 pt-8 pb-8',
         className,
+        'aside',
       )}
     >
-      {stepsArray.map(({ step, title }, index) => {
-        return (
-          <li key={title}>
-            <Step
-              step={step}
-              title={title}
-              onStepChange={() => {
-                if (inputIsValid) setCurrentStep(index);
-              }}
-              className={
-                currentStep === index ? 'bg-blue-100 text-blue-400' : ''
-              }
-            />
-          </li>
-        );
-      })}
-    </ul>
+      <ul
+        className={cn(
+          'flex justify-center gap-4 md:block md:space-y-8 sm:mb-8 md:mb-0',
+          'aside__step-list',
+        )}
+      >
+        {stepsArray.map(({ step, title }, index) => {
+          return (
+            <li key={title}>
+              <Step
+                step={step}
+                title={title}
+                onStepChange={() => {
+                  if (inputIsValid) setCurrentStep(index);
+                }}
+                className={
+                  currentStep === index ? 'bg-blue-100 text-blue-400' : ''
+                }
+              />
+            </li>
+          );
+        })}
+      </ul>
+    </div>
   );
 };
 
