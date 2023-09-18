433374539fa4934a60fa5f55be57d65871a25641
adejolx <dejoladaniel@gmail.com>
2023-09-18 01:42:44 +0100

    refactor: conditionally render success msg

--- src/components/Summary/_components/Completed.tsx
+++ src/components/Summary/_components/Completed.tsx
@@ -1,5 +1,17 @@
+import completedSvg from 'assets/icon-thank-you.svg';
+import { FrontMatter } from 'components';
+
 const Completed = () => {
-  return <></>;
+  return (
+    <div className="h-full grid gap-8 place-items-center">
+      <img src={completedSvg} alt="" />
+      <FrontMatter
+        title="Thank you!"
+        description="Thanks for confirming your subscription! We hope you have fun using our platform. If you ever need support, please feel free to email us at support@loremgaming.com"
+        className="text-center"
+      />
+    </div>
+  );
 };
 
 export { Completed };
